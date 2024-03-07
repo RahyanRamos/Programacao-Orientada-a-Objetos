c91c9538ff326e1735aa1e2acfe7cf9f0d256d7b
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-03-06 20:01:14 -0400

    Adicionando código da aula do dia 28/02/2024

---
+++ Aula_28-02/Banco.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Banco
+    {
+        public int id { get; set; }
+        public int agencia { get; set; }
+        public int conta { get; set;}
+        public string nomeProprietario { get; set;}
+        public double saldo;
+        public void SetSaldo(double valor)
+        {
+            if (valor >= 0)
+            {
+                saldo += valor
+               
+            }
+        }
+
+        public double Deposito(double valor)
+        {
+            if (valor >= 0)
+            {
+                saldo += valor;
+            }
+            else 
+            {
+                Console.WriteLine("Valor inválido!");
+            }
+            return saldo;
+        }
+
+        public double Saque(double valor)
+        {
+            if (valor >= 0)
+            {
+                saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido!");
+            }
+            return saldo;
+        }
+
+        public void Transferir(Banco destino, double valor)
+        {
+            destino.saldo += valor;
+            saldo -= valor;
+        }
+
+    }
+}
---
+++ Aula_28-02/Program.cs
@@ -0,0 +1,45 @@
+﻿// Exercicio 1 - Aula POO 21/02/2024
+
+using Exercicio1;
+
+Banco banco = new Banco();
+
+Console.Write("Informe o seu ID: ");
+int id = int.Parse(Console.ReadLine());
+banco.id = id;
+
+Console.Write("Informe o seu Nome: ");
+string nome = Console.ReadLine();
+banco.nomeProprietario = nome;
+
+Console.Write("Informe o Número da Agência: ");
+int agencia = int.Parse(Console.ReadLine());
+banco.agencia = agencia;
+
+Console.Write("Informe o Número da Conta: ");
+int conta = int.Parse(Console.ReadLine());
+banco.conta = conta;
+
+Console.Write("Informe o Saldo atual da sua conta: ");
+double saldo = double.Parse(Console.ReadLine());
+banco.saldo = saldo;
+
+Console.Write("");
+
+Console.Write("Faça um depósito em sua conta: ");
+double deposito = double.Parse(Console.ReadLine());
+banco.Deposito(deposito);
+Console.WriteLine();
+Console.WriteLine($"Novo saldo: " + banco.saldo);
+
+Console.Write("");
+
+Console.Write("Faça um saque em sua conta: ");
+double saque = double.Parse(Console.ReadLine());
+banco.Saque(saque);
+Console.WriteLine();
+Console.WriteLine($"Novo saldo: " + banco.saldo);
+
+
+
+Console.ReadKey();
