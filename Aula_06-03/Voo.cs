b426448ca04f68253fb3108ff0854f3f7b76d6c2
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-03-06 21:12:13 -0400

    Ceiando arquivo da aulda do dia 06/03/2024

---
+++ Aula_06-03/Voo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_06_03
+{
+    internal class Voo
+    {
+    }
+}
