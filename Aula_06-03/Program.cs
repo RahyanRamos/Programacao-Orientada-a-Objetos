4ec6da446290156ea43300b3d6d0fba5e6e32c08
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-03-06 22:09:00 -0400

    Inserindo parâmetros do Voo

--- Aula_06-03/Program.cs
+++ Aula_06-03/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using Aula_06_03;
+
+DateTime data = DateTime.Now.Date;
+int numero = 5;
+
+Voo(numero, data);
--- Aula_06-03/Voo.cs
+++ Aula_06-03/Voo.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Aula_06_03
 {
-    internal class Voo
+    public class Voo
     {
+        public int numero;
+        public DateOnly data;
+        public TimeOnly hora;
+        public int maxPassageiro;
+        public bool vazio;
+
+        public Voo(int n, DateOnly dt)
+        {
+            numero = n;
+            data = dt;
+        }
+
+
+        //public int ProximoLivre(Voo maxPassageiro, Voo vazio)
+        //{
+
+        //    int poltrona;
+
+        //    return poltrona;
+        //}
     }
 }
