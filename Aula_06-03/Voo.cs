1a8b4d29cb39fe831f29f92d74fc1334984b4524
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-03-13 19:25:09 -0400

    Arrumando atributos da Classe e redefinincdo método construtor

--- Aula_06-03/Voo.cs
+++ Aula_06-03/Voo.cs
@@ -8,16 +8,16 @@ namespace Aula_06_03
 {
     public class Voo
     {
-        public int numero;
-        public DateTime data;
-        public TimeOnly hora;
-        public int maxPassageiro;
-        public bool[] vazio = new bool[10];
+        public int Numero;
+        public DateTime Data;
+        public TimeOnly Hora;
+        public int MaxPassageiro;
+        //public bool[] vazio = new bool[10];
 
         public Voo(int n, DateTime dt)
         {
-            numero = n;
-            data = dt;
+            Numero = n;
+            Data = dt;
         }
 
 
