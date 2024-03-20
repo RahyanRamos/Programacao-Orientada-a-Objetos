eb84434f9da68c65075421271ca7ab4a6c4bb8b5
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-03-20 19:40:32 -0400

    Criando arquivo da aula do dia 20/03

---
+++ Aula_20-03/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_20_03
+{
+    public class Produto
+    {
+        int id;
+        string descricao;
+        string unidade;
+        double preco;
+        int estoque;
+    }
+
+    public void SetPreco
+    {
+
+    }
+}
